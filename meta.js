38ec15fad4ddfd144a80753199175f2faf60412d
听着情歌流泪 <develop@lingtal.com>
2017-12-20 18:13:08 +0800

    browserslist 判断

--- meta.js
+++ meta.js
@@ -133,7 +133,7 @@ module.exports = {
     autoInstall: {
       type: 'list',
       message:
-        'Should we run `npm install` for you after the project has been created? (recommended)',
+        '项目创建后，是否自动安装依赖文件?',
       choices: [
         {
           name: 'Yes, use NPM',
